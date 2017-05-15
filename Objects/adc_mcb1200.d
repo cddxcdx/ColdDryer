2b4f81d29616a3aa268401e9aba0e346be170563
Chen Dongdong <ddchen@titangroupco.com>
2017-05-15 12:35:27 +0800

    Delete Listings and Objects.

--- Objects/adc_mcb1200.d
+++
@@ -1,8 +0,0 @@
-.\objects\adc_mcb1200.o: C:\Keil_v5\ARM\PACK\Keil\MCB1200_BSP\1.0.0\Common\ADC_MCB1200.c
-.\objects\adc_mcb1200.o: C:\Keil_v5\ARM\PACK\Keil\LPC1200_DFP\1.0.0\Device\Include\LPC12xx\LPC122x.h
-.\objects\adc_mcb1200.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.2.0\CMSIS\Include\core_cm0.h
-.\objects\adc_mcb1200.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\adc_mcb1200.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.2.0\CMSIS\Include\core_cmInstr.h
-.\objects\adc_mcb1200.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.2.0\CMSIS\Include\core_cmFunc.h
-.\objects\adc_mcb1200.o: C:\Keil_v5\ARM\PACK\Keil\LPC1200_DFP\1.0.0\Device\Include\LPC12xx\system_LPC122x.h
-.\objects\adc_mcb1200.o: C:\Keil_v5\ARM\PACK\Keil\MDK-Middleware\6.1.1\Board\Board_ADC.h
--- Objects/startup_lpc122x.d
+++
@@ -1,1 +0,0 @@
-.\objects\startup_lpc122x.o: RTE\Device\LPC1225FBD48_301\startup_LPC122x.s
